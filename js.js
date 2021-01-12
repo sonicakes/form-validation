9932828ec21b8971d4c9dba2d4362e062ecf55e4
Sonia Parker <sonia.parker@ping-works.com.au>
2021-01-12 14:46:52 +1100

    add checking empty fields validation

--- js.js
+++ js.js
@@ -1,6 +1,29 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
+
+            $("#submit").click(function(e){
+                e.preventDefault();
+                checkEmpty("#firstName");
+                checkEmpty("#lastName");
+                checkEmpty("#email");
+                checkEmpty("#password");
+    
+            });
+            
+
+       function checkEmpty (fieldName) {
+        if ($(fieldName).attr("value") === undefined) {
+            console.log("the " + fieldName + " field is empty");
+            let newValue = fieldName.substring(1);
+            console.log(newValue);
+        
+            $("." + newValue).text("Sorry, the " + $(fieldName).attr("name") + " cannot be empty");
+
+        }
+       }
+
+  
 });
 
 //todo
