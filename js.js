b03bdb9e88fd8ca8d9aced3c1ca9fc8ebe5b6ba3
Sonia Parker <sonia.parker@ping-works.com.au>
2021-01-12 15:31:55 +1100

    add correct empty validation and email field validation

--- js.js
+++ js.js
@@ -1,31 +1,52 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
 
-            $("#submit").click(function(e){
-                e.preventDefault();
-                checkEmpty("#firstName");
-                checkEmpty("#lastName");
-                checkEmpty("#email");
-                checkEmpty("#password");
-    
-            });
-            
+  $("#submit").click(function (e) {
+    e.preventDefault();
+    checkEmpty("#firstName");
+    checkEmpty("#lastName");
+    checkEmpty("#email");
+    checkEmpty("#password");
+    validateEmail("#email");
+  });
 
-       function checkEmpty (fieldName) {
-        if ($(fieldName).attr("value") === undefined) {
-            console.log("the " + fieldName + " field is empty");
-            let newValue = fieldName.substring(1);
-            console.log(newValue);
-        
-            $("." + newValue).text("Sorry, the " + $(fieldName).attr("name") + " cannot be empty");
+  function checkEmpty(fieldName) {
+    let b = $(fieldName).val();
+    console.log(b);
 
-        }
-       }
+    let newValue = fieldName.substring(1);
 
-  
+    if (b === undefined || b === "") {
+      console.log(b);
+
+      console.log("the " + fieldName + " field is empty", "value: " + b);
+      console.log(newValue);
+
+      $("." + newValue).text(
+        "Sorry, the " + $(fieldName).attr("name") + " cannot be empty."
+      );
+    } else {
+      $("." + newValue)
+        .text("Yay! Good stuff!")
+        .addClass("success");
+    }
+  }
+
+  function validateEmail(mail) {
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    console.log(mail);
+    let c = $(mail).val();
+    console.log(c);
+
+    if (c.match(mailformat)) {
+      $(".error-msg.email-validity")
+        .text("Congrats! This is a valid email address!")
+        .addClass("success");
+    } else {
+      $(".error-msg.email-validity").text(
+        "Sorry! This is not a valid email address!"
+      );
+    }
+  }
 });
-
-//todo
-//1. check if values of fname,lname and pswd are empty, and if yes, display relevant msgs
-//2. validate email & display msg
