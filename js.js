15d1adde8e90b645121429927550fe4130565bd6
Sonia Parker <sonia.parker@ping-works.com.au>
2021-01-12 12:16:18 +1100

    add basic layout

---
+++ js.js
@@ -0,0 +1,8 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+//todo
+//1. check if values of fname,lname and pswd are empty, and if yes, display relevant msgs
+//2. validate email & display msg
